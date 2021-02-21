fe4c8485969acb87ff4d98e9513d1742afea3648
Raulpemol <UO263743@uniovi.es>
2021-02-21 16:47:44 +0100

    Hello world

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const hostname = 'localhost';
 const port = 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Server running at http://${hostname}:${port}`))
