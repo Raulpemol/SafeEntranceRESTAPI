0b725a65bf6fb7f1d4a6c6961f0559cd80b5fddb
Raulpemol <UO263743@uniovi.es>
2021-02-22 15:58:44 +0100

    Test app service in US

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hostname = process.env.WEBSITE_HOSTNAME || 'localhost';
 const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! The app is now running CD/CI')
+    res.send('Hello World! The app is now running CD/CI from US')
 })
 
 app.listen(port, () => console.log(`Server running at http://${hostname}:${port}`))
