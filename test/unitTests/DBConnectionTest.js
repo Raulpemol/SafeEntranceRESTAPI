e8f8fb7077957d7ce64d97e2ed845e58eb94d8ef
Raulpemol <UO263743@uniovi.es>
2021-03-12 17:21:45 +0100

    Unit tests for adding places

--- test/unitTests/DBConnectionTest.js
+++ test/unitTests/DBConnectionTest.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const getDBConnection = require('../../modules/DBManager');
+const mongoose = require('mongoose');
+const Place = require('../../modules/api/models/place');
 
 const URI = "mongodb+srv://admin:VXm8s3Up4BLK86HS@safeentrancebd.riy03.mongodb.net/SafeEntranceBD_test?retryWrites=true&w=majority";
 
@@ -7,9 +9,50 @@ before(function(){
     getDBConnection(URI);
 });
 
-describe('Database connection', function(){
-    describe('BD Manager should return true if the connection was stablished', async function(){
-        var dbConnectionResult = await getDBConnection();
-        assert.strictEqual(dbConnectionResult, true);
+describe('Initialize collection places', function(){
+    it('should empty the collection', async function(){
+        Place.deleteMany({}, function(err){
+            if(err){
+                assert.fail();
+            }
+            else{
+                Place.find({}, function(err2, places){
+                    if(err2){
+                        assert.fail();
+                    }
+                    else{
+                        assert.strictEqual(places.length, 0);
+                    }
+                });
+            }
+        })
+    });
+});
+
+describe('Inserting a correct place', function(){
+    it('should add a place to the collection', async function(){
+        const place = new Place({
+            _id: new mongoose.Types.ObjectId(),
+            name: "test name",
+            address : "test address",
+            capacity : "1"
+        });
+
+        await place.save().then(result => {
+            assert.strictEqual(result.name, "test name");
+            assert.strictEqual(result.address, "test address");
+            assert.strictEqual(result.capacity, 1);
+        }).catch(err => {
+            console.log(err);
+            assert.fail();
+        });
+    describe('The collection must have', function(){
+        it('only one place', function(){
+            const db = mongoose.connection;
+            db.collection("places", function(err, collection){
+                assert.strictEqual(collection.count(), 1);
+            });
+        });
+    });
     });
 });
