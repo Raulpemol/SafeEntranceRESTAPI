afa0e4a4016bf426e80bf632a424bb62cdf5dcdd
Raulpemol <UO263743@uniovi.es>
2021-03-28 11:20:34 +0200

    The system does not allow duplicated places

--- modules/api/routes/place.js
+++ modules/api/routes/place.js
@@ -5,18 +5,44 @@ const mongoose = require('mongoose');
 const Place = require('../models/place');
 
 route.post('/addPlace', async (req, res) => {
-    const place = new Place({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        address : req.body.address,
-        capacity : req.body.capacity
-    });
-    place.save().then(result => {
-            console.log(result);
-            res.status(201).json(place._id);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json("Error saving the new place");
+    Place.findOne({name: req.body.name}, function(err, placeByName){
+        if(err){
+            console.log(err);
+            res.status(500).json("Error accessing the database");
+        }
+        else{
+            if(placeByName != null){
+                res.status(400).json(req.body.name + " ya fue registrado en el sistema previamente");
+            }
+            else{
+                Place.findOne({address: req.body.address}, function(err, placeByAddress){
+                    if(err){
+                        console.log(err);
+                        res.status(500).json("Error accessing the database");
+                    }
+                    else{
+                        if(placeByAddress != null){
+                            res.status(400).json("Ya se ha registrado un local en la dirección " + req.body.address);
+                        }
+                        else{
+                            const place = new Place({
+                                _id: new mongoose.Types.ObjectId(),
+                                name: req.body.name,
+                                address : req.body.address,
+                                capacity : req.body.capacity
+                            });
+                            place.save().then(result => {
+                                    console.log(result);
+                                    res.status(201).json(place._id);
+                            }).catch(err => {
+                                console.log(err);
+                                res.status(500).json("Error saving the new place");
+                            });
+                        }
+                    }
+                });
+            }
+        }
     });
 });
 
