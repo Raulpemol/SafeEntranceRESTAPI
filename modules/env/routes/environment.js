e68c6bd5d3b562ccba4d41f1bcd672827f8decd3
Raulpemol <UO263743@uniovi.es>
2021-04-02 20:53:56 +0200

    setidbp method added

--- modules/env/routes/environment.js
+++ modules/env/routes/environment.js
@@ -16,4 +16,32 @@ route.get('/idbp', async (req, res) => {
     }
 });
 
+route.post('/setidbp', async (req, res) => {
+    try{
+        var token = req.headers['token'] || req.body.token || req.query.token;
+        jwt.verify(token, 'encrypted', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403);
+                res.json({ 
+                    acceso : false, 
+                    error: 'Token invalido o caducado'
+                });
+            } 
+            else {
+                const value = req.body.value;
+                if(value == undefined || value == null || value == ""){
+                    res.status(400).json("Incorrect value");
+                }
+                else{
+                    process.env.INFECT_DAYS_BEFORE_PCR = value;
+                    res.status(200).json("Value changed");
+                }
+            }
+        });
+    }
+    catch{
+        res.status(400).json("Incorrect parameter format");
+    }
+});
+
 module.exports = route;
