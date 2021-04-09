92f04a1a1f548282611a403932bbb5c65768a186
Raulpemol <UO263743@uniovi.es>
2021-04-09 17:42:39 +0200

    Added api methods for DAPI variable

--- modules/env/routes/environment.js
+++ modules/env/routes/environment.js
@@ -70,4 +70,69 @@ route.post('/setidbp', async (req, res) => {
     }
 });
 
+route.get('/dapi', async (req, res) => {
+    try{
+        EnvVariable.findOne({name: "dapi"}, function(err, dapi){
+            if(err){
+                console.log(err);
+                res.status(500).json("Error accessing the database");
+            }
+            else{
+                res.status(200).json(dapi.value);
+            }
+        });
+    }
+    catch{
+        res.status(400).json("Incorrect parameter format");
+    }
+});
+
+route.post('/setdapi', async (req, res) => {
+    try{
+        var token = req.headers['token'] || req.body.token || req.query.token;
+        jwt.verify(token, 'encrypted', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403);
+                res.json({ 
+                    acceso : false, 
+                    error: 'Token invalido o caducado'
+                });
+            } 
+            else {
+                EnvVariable.findOne({name: "dapi"}, function(err, dapi){
+                    if(err){
+                        console.log(err);
+                        res.status(500).json("Error accessing the database");
+                    }
+                    else{
+                        const value = req.body.value;
+                        if(dapi == null){
+                            dapi = new EnvVariable({
+                                _id: new mongoose.Types.ObjectId(),
+                                name: "dapi",
+                                value: value
+                            });
+                            
+                        }
+                        else{
+                            dapi.value = value;
+                        }
+
+                        dapi.save().then(result => {
+                            console.log(result);
+                            res.status(201).json("Value changed");
+                        }).catch(err => {
+                            console.log(err);
+                            res.status(500).json("Error updating the value");
+                        });
+                    }
+                });
+            }
+        });
+    }
+    catch{
+        res.status(400).json("Incorrect parameter format");
+    }
+});
+
 module.exports = route;
