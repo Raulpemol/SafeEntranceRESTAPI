c416f58720109089ff4f02b28626a3240d8d80e0
Raulpemol <UO263743@uniovi.es>
2021-05-24 17:09:54 +0200

    Occupation field to places and added scan method

--- modules/api/models/place.js
+++ modules/api/models/place.js
@@ -4,7 +4,8 @@ const placeSchema = new mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     name: String,
     address: String,
-    capacity: Number
+    capacity: Number,
+    occupation: Number
 });
 
 module.exports = Place = mongoose.model('place', placeSchema);
